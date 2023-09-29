4c9b49b061dd25b2754c3fdb4684a26e215598d0
liamirilaaa <144470903+liamirilaaa@users.noreply.github.com>
2023-09-29 19:46:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -43,4 +43,15 @@ async def add(ctx, left: int, right: int):
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
+@bot.command()
+async def roll(ctx, dice: str):
+    """Rolls a dice in NdN format."""
+    try:
+        rolls, limit = map(int, dice.split('d'))
+    except Exception:
+        await ctx.send('Format has to be in NdN!')
+        return
+
+    result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
+    await ctx.send(result)
 
