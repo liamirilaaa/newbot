e7d16b9de9cb7d0356da5643f285d53e9b0bcfaa
liamirilaaa <144470903+liamirilaaa@users.noreply.github.com>
2023-10-06 19:32:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 import random
+import os
+import requests
 from discord.ext import commands
 
 intents = discord.Intents.default()
@@ -43,6 +45,7 @@ async def add(ctx, left: int, right: int):
 async def choose(ctx, *choices: str):
     """Chooses between multiple choices."""
     await ctx.send(random.choice(choices))
+
 @bot.command()
 async def roll(ctx, dice: str):
     """Rolls a dice in NdN format."""
@@ -55,3 +58,36 @@ async def roll(ctx, dice: str):
     result = ', '.join(str(random.randint(1, limit)) for r in range(rolls))
     await ctx.send(result)
 
+@bot.command()
+async def mem(ctx):
+    img_name = random.choice(os.listdir('images'))
+    with open(f'images/{img_name}', 'rb') as f:
+        picture = discord.File(f)
+   # Можем передавать файл как параметр!
+    await ctx.send(file=picture)
+
+def get_duck_image_url():    
+    url = 'https://random-d.uk/api/random'
+    res = requests.get(url)
+    data = res.json()
+    return data['url']
+
+
+@bot.command('duck')
+async def duck(ctx):
+    '''По команде duck вызывает функцию get_duck_image_url'''
+    image_url = get_duck_image_url()
+    await ctx.send(image_url)
+
+def get_fox_image_url():    
+    url = 'https://randomfox.ca/floof/'
+    res = requests.get(url)
+    data = res.json()
+    return data['image']
+
+
+@bot.command('fox')
+async def fox(ctx):
+    '''По команде fox вызывает функцию get_fox_image_url'''
+    image_url = get_fox_image_url()
+    await ctx.send(image_url)
